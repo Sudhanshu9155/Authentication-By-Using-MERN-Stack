72580249b87326e3fe8c98512a6351413d63f2bb
Sudhanshu Kumar <149174300+Sudhanshu9155@users.noreply.github.com>
2026-08-16 20:36:45 +0530

    auth

---
+++ src/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userScheme = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: [true, "Username already exists"],
+  },
+  email:{
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: [true, "Email already exists"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  }
+})
+
+
+const userModel = mongoose.model("User", userScheme); 
+
+export default userModel;
