f7fce110f6563f2eb2069622f15291d0a53b3b5f
Sudhanshu Kumar <149174300+Sudhanshu9155@users.noreply.github.com>
2026-08-17 15:09:04 +0530

    refresh token and logout add

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,8 @@ import * as authController from "../controllers/auth.controller.js";
 const authRouter = Router();
 
 authRouter.post('/register', authController.register);
+authRouter.get('/login', authController.login);
+authRouter.post('/refresh-token', authController.refreshToken);
+authRouter.get('/logout', authController.logout);
 
 export default authRouter;
